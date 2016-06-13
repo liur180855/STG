03b888400e32880dca8aba359e7a8bbe147c6aab
Liu <rliu2@netscout.com>
2016-06-13 17:03:26 -0500

    dafdfasd

---
+++ app/singleHouse/mapSearchBox.js
@@ -0,0 +1,4 @@
+function initMap() {
+	var input = document.getElementById('house.address');
+    var searchBox = new google.maps.places.SearchBox(input);
+}
--- app/singleHouse/singleHouse.controller.js
+++ app/singleHouse/singleHouse.controller.js
@@ -6,58 +6,12 @@
 	function Controller(dataService,rentalFactory) {
         var vm = this;
         vm.postInfo = postInfo;
-        /*
-        vm.longtermEnable = longtermEnable(vm.todtlongterm,vm.todt);
 		
-
-		function longtermEnable(longterm,todt){
-        	var todtenable = document.getElementById(todt);
-        	if (longterm==true){
-        		todtenable.setAttribute("disabled", "disabled");
-				
-        	}else{
-				todtenable.removeAttribute("disabled");
-        	}
-        }
-<<<<<<< HEAD
-        vm.open2 = open2;
-
-		vm.today = function() {
-		    vm.dt = new Date();
-		};
-		vm.dateOptions = {
-		    dateDisabled: disabled,
-		    formatYear: 'yy',
-		    maxDate: new Date(2020, 5, 22),
-		    minDate: new Date(),
-		    startingDay: 1
-		  };
-		 function disabled(data) {
-		    var date = data.date,
-		      mode = data.mode;
-		    return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
-		  }
-
-		    vm.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
- vm.format = vm.formats[0];
-  vm.altInputFormats = ['M!/d!/yyyy'];
-		vm.today();
-
-
-	function open2() {
-
-		console.log(vm.house.dt);
-    vm.popup2.opened = true;
-  };
-
-		vm.popup2 = {
-    opened: false
-  };
-  */
+		
 
 
 		function postInfo(){
-			
+			console.log(vm.house.roommateGender);
 			vm.house.rentalTime = rentalFactory.getRentalTime(vm.fromdt,vm.todt,vm.todtlongterm);
 			console.log(vm.house.rentalTime);
 			if((vm.todt !== undefined ||vm.todtlongterm)&& vm.fromdt !== undefined && vm.house.address !== undefined && vm.house.price !== undefined){
